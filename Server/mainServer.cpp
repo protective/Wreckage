0077c77ab66c6bf38416dc5f1e0212e3ac98b39d
Karsten Jakobsen <karstenjjakobsen@gmail.com>
2014-10-13 20:20:09 +0200

    Initial commit

---
+++ Server/mainServer.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   mainServer.cpp
+ * Author: karsten
+ *
+ * Created on 13. oktober 2014, 19:55
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
